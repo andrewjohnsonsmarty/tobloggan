94391ca2ebfcf61527bb68eb2e3849bfb6a776a5
Xan Johnson <xan@smarty.com>
2024-10-03 11:47:39 -0600

    Finish station 1! Hooray

--- code/stations/01_source_reader.go
+++ code/stations/01_source_reader.go
@@ -1,10 +1,36 @@
 package stations
 
-//type SourceReader struct {
-//}
+import (
+	"io/fs"
 
-//func (this *SourceReader) Do(input any, output func(v any)) {
-//    TODO: given a contracts.SourceFilePath, read its contents and emit contracts.SourceFile
-//    input: contracts.SourceFilePath
-//    output: contracts.SourceFile, or error from fs.ReadFile, or input w/ unrecognized type
-//}
+	"tobloggan/code/contracts"
+)
+
+type SourceReader struct {
+	files fs.FS
+}
+
+func NewSourceReader(files fs.FS) contracts.Station {
+	return &SourceReader{files: files}
+}
+
+func (this *SourceReader) Do(input any, output func(v any)) {
+	switch input := input.(type) {
+
+	case contracts.SourceFilePath:
+		file, err := fs.ReadFile(this.files, string(input))
+		if err != nil {
+			//output(fmt.Errorf("error reading file: %w", err))
+			output(err)
+			return
+		}
+		output(contracts.SourceFile(file))
+
+	default:
+		output(input)
+	}
+
+	//   TODO: given a contracts.SourceFilePath, read its contents and emit contracts.SourceFile
+	//    input: contracts.SourceFilePath
+	//    output: contracts.SourceFile, or error from fs.ReadFile, or input w/ unrecognized type
+}
--- code/stations/01_source_reader_test.go
+++ code/stations/01_source_reader_test.go
@@ -1,4 +1,61 @@
 package stations
 
-//func (this *SourceReaderFixture) TestSourceFileContentReadFromDiskAndEmitted() {}
-//func (this *SourceReaderFixture) TestIOError() {}
+import (
+	"io/fs"
+	"testing"
+	"testing/fstest"
+
+	"tobloggan/code/contracts"
+
+	"github.com/smarty/assertions/should"
+	"github.com/smarty/gunit"
+)
+
+// `Emit unrecognizeed types
+
+func TestSourceReaderFixture(t *testing.T) {
+	gunit.Run(new(SourceReaderFixture), t)
+}
+
+type SourceReaderFixture struct {
+	*gunit.Fixture
+	files   fstest.MapFS
+	station contracts.Station
+	outputs []any
+}
+
+func (this *SourceReaderFixture) Setup() {
+	this.files = fstest.MapFS{}
+	this.station = NewSourceReader(this.files)
+}
+
+func (this *SourceReaderFixture) output(v any) {
+	this.outputs = append(this.outputs, v)
+}
+
+func (this *SourceReaderFixture) TestIOError() {
+	this.station.Do(contracts.SourceFilePath("asdfasdf"), this.output)
+	this.So(len(this.outputs), should.Equal, 1)
+	this.So(this.outputs[0], should.Wrap, fs.ErrNotExist)
+
+}
+
+func (this *SourceReaderFixture) TestSourceFileContentReadFromDiskAndEmitted() {
+	this.files["src/article.md"] = &fstest.MapFile{Data: []byte("article content")}
+	this.station.Do(contracts.SourceFilePath("src/article.md"), this.output)
+	this.So(len(this.outputs), should.Equal, 1)
+	this.So(this.outputs[0], should.Equal, contracts.SourceFile("article content"))
+}
+
+func (this *SourceReaderFixture) TestUnrecognizedType() {
+	this.station.Do(420, this.output)
+	this.So(len(this.outputs), should.Equal, 1)
+	this.So(this.outputs[0], should.Equal, 420)
+}
+
+func (this *SourceReaderFixture) TestReceiveContractsSourceFile() {
+	this.files["src/article.md"] = &fstest.MapFile{Data: []byte("article content")}
+	this.station.Do(contracts.SourceFilePath("src/article.md"), this.output)
+	this.So(len(this.outputs), should.Equal, 1)
+	this.So(this.outputs[0], should.HaveSameTypeAs, contracts.SourceFile{})
+}
