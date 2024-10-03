c62c8f5c87bf296058b56552236461315679989c
Andrew Johnson <andrew.johnson@smarty.com>
2024-10-03 15:16:09 -0600

    Article validator done

--- code/stations/03_article_validator.go
+++ code/stations/03_article_validator.go
@@ -1,16 +1,40 @@
 package stations
 
 import (
+	"fmt"
+
+	"tobloggan/code/contracts"
 	"tobloggan/code/set"
 )
 
-//type ArticleValidator struct {
-//}
+type ArticleValidator struct {
+	seenSlugs []string
+}
 
-//func (this *ArticleValidator) Do(input any, output func(any)) {
-//    TODO: given a contracts.Article, validate the Slug and the Title fields and emit the contracts.Article (or an error)
-//    input: contracts.Article
-//    output: contracts.Article (or error)
-//}
+func NewArticleValidator() *ArticleValidator {
+	return &ArticleValidator{}
+}
+func (this *ArticleValidator) Do(input any, output func(any)) {
+	switch input := input.(type) {
+	case contracts.Article:
+		for _, r := range input.Slug {
+			if !validSlugCharacters.Contains(r) {
+				output(fmt.Errorf("slug contains invalid character: %s", string(r)))
+				return
+			}
+		}
+		if len(input.Title) == 0 {
+			output(fmt.Errorf("title is empty"))
+			return
+		}
+
+		output(input)
+	default:
+		output(input)
+	}
+	//    TODO: given a contracts.Article, validate the Slug and the Title fields and emit the contracts.Article (or an error)
+	//    input: contracts.Article
+	//    output: contracts.Article (or error)
+}
 
 var validSlugCharacters = set.New([]rune("abcdefghijklmnopqrstuvwxyz0123456789-/")...)
--- code/stations/03_article_validator_test.go
+++ code/stations/03_article_validator_test.go
@@ -1,6 +1,53 @@
 package stations
 
-//func (this *ArticleValidatorFixture) TestValidArticle() {}
-//func (this *ArticleValidatorFixture) TestInvalidSlugs() {}
-//func (this *ArticleValidatorFixture) TestInvalidTitles() {}
+import (
+	"testing"
+
+	"tobloggan/code/contracts"
+
+	"github.com/smarty/assertions/should"
+	"github.com/smarty/gunit"
+)
+
+func TestArticleValidatorFixture(t *testing.T) {
+	gunit.Run(new(ArticleValidatorFixture), t)
+}
+
+type ArticleValidatorFixture struct {
+	StationFixture
+}
+
+func (this *ArticleValidatorFixture) Setup() {
+	this.station = NewArticleValidator()
+}
+
+func (this *ArticleValidatorFixture) TestValidArticle() {
+	this.do(articleExpected)
+	this.So(len(this.outputs), should.Equal, 1)
+	this.So(this.outputs[0], should.HaveSameTypeAs, contracts.Article{})
+}
+
+func (this *ArticleValidatorFixture) TestInvalidSlugs() {
+	article.Slug = "/article/1!"
+	this.do(article)
+	this.So(len(this.outputs), should.Equal, 1)
+	this.So(this.outputs[0], should.BeError)
+}
+
+func (this *ArticleValidatorFixture) TestInvalidTitles() {
+	article.Title = ""
+	article.Slug = "/article/1"
+	this.do(article)
+	this.So(len(this.outputs), should.Equal, 1)
+	this.So(this.outputs[0], should.BeError)
+}
+
 //func (this *ArticleValidatorFixture) TestSlugsMustBeUnique() {}
+
+var article = contracts.Article{
+	Draft: false,
+	Slug:  "/article/1",
+	Title: "Article 1",
+	Date:  timeExpected,
+	Body:  "The contents of article 1.",
+}
