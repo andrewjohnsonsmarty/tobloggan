ecf374e9d6275c930f7cf3eee5db24c8d0b3e325
Andrew Johnson <andrew.johnson@smarty.com>
2024-10-03 14:59:23 -0600

    Article Parser is done

--- code/stations/02_article_parser.go
+++ code/stations/02_article_parser.go
@@ -1,11 +1,42 @@
 package stations
 
-//type ArticleParser struct{}
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
 
-//func (this *ArticleParser) Do(input any, output func(any)) {
-//    TODO: given a contracts.SourceFile, parse the JSON metadata and save the body on a contracts.Article.
-//    input: contracts.SourceFile
-//    input: contracts.Article
-//}
+	"tobloggan/code/contracts"
+)
+
+const divider = "+++"
+
+type ArticleParser struct{}
+
+func (this *ArticleParser) Do(input any, output func(any)) {
+	switch input := input.(type) {
+	case contracts.SourceFile:
+		strInput := strings.Split(string(input), divider)
+		if len(strInput) != 2 {
+			output(fmt.Errorf("invalid input: %s", input))
+			return
+		}
+		content := strInput[0]
+		body := strings.TrimSpace(strInput[1])
+		article := contracts.Article{}
+		err := json.Unmarshal([]byte(content), &article)
+		if err != nil {
+			output(err)
+			return
+		}
+		article.Body = body
+		output(article)
+	default:
+		output(input)
+	}
+
+	//    TODO: given a contracts.SourceFile, parse the JSON metadata and save the body on a contracts.Article.
+	//    input: contracts.SourceFile
+	//    input: contracts.Article
+}
 
 //var errMalformedContent = errors.New("malformed content")
--- code/stations/02_article_parser_test.go
+++ code/stations/02_article_parser_test.go
@@ -1,8 +1,56 @@
 package stations
 
-//func (this *ArticleParserFixture) TestArticleMetaAndContentReadFromDiskAndEmitted() {}
-//func (this *ArticleParserFixture) TestMissingDivider() {}
-//func (this *ArticleParserFixture) TestMalformedMetadata() {}
+import (
+	"encoding/json"
+	"testing"
+	"time"
+
+	"tobloggan/code/contracts"
+
+	"github.com/smarty/assertions/should"
+	"github.com/smarty/gunit"
+)
+
+func TestArticleParserFixture(t *testing.T) {
+	gunit.Run(new(ArticleParserFixture), t)
+}
+
+type ArticleParserFixture struct {
+	StationFixture
+}
+
+func (this *ArticleParserFixture) Setup() {
+	this.station = &ArticleParser{}
+}
+
+func (this *ArticleParserFixture) TestBasicReturn() {
+	this.do(article1Content)
+	this.So(len(this.outputs), should.Equal, 1)
+}
+
+func (this *ArticleParserFixture) TestArticleMetaAndContentReadFromDiskAndEmitted() {
+	this.do(contracts.SourceFile(article1Content))
+	this.So(this.outputs[0], should.Equal, articleExpected)
+}
+
+func (this *ArticleParserFixture) TestMissingDivider() {
+	this.do(contracts.SourceFile("nothing"))
+	this.So(this.outputs[0], should.BeError)
+}
+
+func (this *ArticleParserFixture) TestMalformedMetadata() {
+	this.do(contracts.SourceFile("malformed+++hello"))
+	this.So(this.outputs[0], should.HaveSameTypeAs, &json.SyntaxError{})
+}
+
+var timeExpected, _ = time.Parse("2006-01-02T15:04:05Z", "2024-09-04T00:00:00Z")
+var articleExpected = contracts.Article{
+	Draft: false,
+	Slug:  "/article/1",
+	Title: "Article 1",
+	Date:  timeExpected,
+	Body:  "The contents of article 1.",
+}
 
 const article1Content = `{
 	"date": "2024-09-04T00:00:00Z",
